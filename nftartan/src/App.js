40faf45805581516a5aab800610860bcf022e933
AndGao015 <gao.andrew.ag.ag.ag@gmail.com>
2021-12-28 13:20:29 -0800

    added scuffed page switch button

--- nftartan/src/App.js
+++ nftartan/src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "./components/Navbar";
 import Display from "./pages/Display";
 import LandingPage from "./pages/LandingPage";
@@ -6,6 +6,14 @@ import { Helmet } from "react-helmet";
 import "./App.css";
 
 function App() {
+  const [switchPage, setSwitch] = useState(false)
+  let ActivePage = null
+  if (switchPage) {
+    ActivePage = <LandingPage />
+  } else {
+    ActivePage = <Display />
+  }
+
   return (
     <div>
       {/* using React Helmet Library to set placeholder background color */}
@@ -16,7 +24,12 @@ function App() {
       <div className="navbar-top">
         <Navbar />
       </div>
-      <LandingPage />
+
+      <button onClick={() => setSwitch(!switchPage)}>
+        click me to switch page
+      </button>
+
+      {ActivePage}
       {/* <Display /> */}
     </div>
   );
