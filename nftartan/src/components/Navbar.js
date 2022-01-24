cd132b1c680a7231de0b1bf447ee10c8dac8487e
Angus Yiu <angusyiuky@gmail.com>
2022-01-24 00:05:17 -0500

    phantom integration

--- nftartan/src/components/Navbar.js
+++ nftartan/src/components/Navbar.js
@@ -1,25 +1,69 @@
-import React, { Component } from 'react'
-import { MenuItems } from './MenuItems'
-import '../styles/Navbar.css'
+import { useState } from "react";
+import { MenuItems } from "./MenuItems";
+import "../styles/Navbar.css";
 
-class Navbar extends Component {
-  render() {
-    return (
-      <nav className="NavbarItems">
-        <ul>
-          {MenuItems.map((item, index) => {
-            return (
-              <li key={index}>
-                <a className={item.cName} href={item.url}>
-                  {item.title}
-                </a>
-              </li>
-            )
-          })}  
-        </ul>
-      </nav>
-    )
-  }
-}
+const Navbar = () => {
+  const [publicKey, setPublicKey] = useState();
+
+  const getProvider = () => {
+    if ("solana" in window) {
+      const provider = window.solana;
+      if (provider.isPhantom) {
+        return provider;
+      }
+    }
+    window.open("https://phantom.app/", "_blank");
+  };
+
+  const connectPhantom = async () => {
+    try {
+      const resp = await window.solana.connect();
+      setPublicKey(resp.publicKey.toString());
+    } catch (err) {
+      console.log(err.message);
+    }
+  };
+
+  const Connect = () => {
+    if (window.solana && window.solana.isConnected) {
+      return (
+        <div
+          style={{
+            color: "white",
+            fontSize: "16px",
+            fontFamily: "Trebuchet MS",
+            textAlign: "right",
+          }}
+        >
+          For testing purposes
+          <h3>{window.solana.publicKey.toString()}</h3>
+        </div>
+      );
+    } else {
+      return (
+        <button onClick={connectPhantom} className="navbar-connect-button">
+          Connect Phantom Wallet
+        </button>
+      );
+    }
+  };
+
+  return (
+    <div className="NavbarItems">
+      <ul>
+        {MenuItems.map((item, index) => {
+          return (
+            <li key={index}>
+              <a className={item.cName} href={item.url}>
+                {item.title}
+              </a>
+            </li>
+          );
+        })}
+      </ul>
+      <Connect />
+    </div>
+  );
+};
 
 export default Navbar;
